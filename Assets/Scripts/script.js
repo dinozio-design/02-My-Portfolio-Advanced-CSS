4600295ed2aabbc20c7bb143516266fe97a6ce2c
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-05-01 00:17:37 -0400

    i changed a whole lot of things, and feel that I am still at the same spot

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -1,13 +1,18 @@
 // this script is to set up the toggle switch for showing more info on each card
 
-const para = document.querySelector("seeMore");
 
-para = addEventListener ("click", seeMore);
+let btn_1 = document.getElementById("#btn-1");
+
+btn_1.addEventListener("click", seeMoreInfo);
 
 
-function seeMore() {
-    var textVisOff = document.getElementsByClassName ("seeMore");
-    if (textVisOff.style.display === "none") {
-        textVisOff.style.display = "block";
+
+function seeMoreInfo(){
+    let seeMore_1 = document.querySelector("#seeMore-1");
+    console.log(seeMore_1.style.display);
+    if (seeMore_1.style.display === "none"){
+        seeMore_1.style.display = "block";
+    } else {
+        seeMore_1.style.display = "none";
     }
 }
